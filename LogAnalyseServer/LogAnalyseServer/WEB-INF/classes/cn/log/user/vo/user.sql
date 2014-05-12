fb6fd5c87a2f7523a30a6cb89b973b8b8799b488
zouqone <zou595895@126.com>
2014-05-12 21:58:09 +0800

    build path

---
+++ LogAnalyseServer/LogAnalyseServer/WEB-INF/classes/cn/log/user/vo/user.sql
@@ -0,0 +1,10 @@
+create table user (
+id int not null primary key auto_increment,
+username varchar(40) null,
+password varchar(20) null,
+age int null,
+sex varchar(10) null,
+address varchar(100) null,
+Info varchar(200) null,
+email varchar(50) null,
+);
